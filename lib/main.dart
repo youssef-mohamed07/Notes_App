c2f192eafb07192487bdff81692d47cda1251034
youssef-mohamed-07 <youssefmohamedahmed2004.com>
2025-08-04 17:53:53 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/screens/home_screen.dart';
+
+void main() {
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
