0eea8bd76e76009e7eee47351ab5a2be6d1a7ffe
youssef-mohamed-07 <youssefmohamedahmed2004.com>
2025-08-05 17:02:07 +0300

    change Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/screens/home_screen.dart';
+import 'package:notes_app/views//home_screen.dart';
 
 void main() {
   runApp(NotesApp());
@@ -10,9 +10,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      theme: ThemeData.dark(),
+      home: const HomeScreen(),
     );
   }
 }
