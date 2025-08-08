bb03e9fd7c134d6d859b3fcc1ceca02f2f195d8e
youssef-mohamed-07 <youssefmohamedahmed2004.com>
2025-08-08 15:40:54 +0300

    add Hive and make init

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'dart:ui';
 
 const kPrimaryColor=const Color(0xff65FCD7);
+const kNoteBox= 'notes_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/home_view.dart'; // ✅ Fixed double slashes
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/views/home_view.dart';
 
-void main() {
-  runApp(const NotesApp()); // ✅ Added `const` for optimization
+void main() async{
+  await Hive.initFlutter();
+  await Hive.openBox(kNoteBox);
+  runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
